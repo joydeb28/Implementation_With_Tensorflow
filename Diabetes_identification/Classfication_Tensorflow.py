4d593999797a4d6186ad78b838af0a8bd99c90c1
Joy <joydebmondal28@gmail.com>
2018-08-24 13:12:39 +0530

    files added

---
+++ Diabetes_identification/Classfication_Tensorflow.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python3
+import pandas as pd
+diabetes = pd.read_csv('pima-indians-diabetes.csv')
---
+++ Linear_Regression/linear_regression.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python3
+
+# -*- coding: utf-8 -*-
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+rng = np.random
+
+# Parameters
+learning_rate = 0.01
+training_epochs = 100
+display_step = 50
+
+# Training Data
+import csv
+train_X = []
+train_Y = []
+with open('data/train.csv', newline='') as csvfile:
+     reader = csv.DictReader(csvfile)
+     for row in reader:
+        train_X.append(float(row['x'])/10)
+        train_Y.append(float(row['y'])/10)
+
+test_X = []
+test_Y = []
+with open('data/test.csv', newline='') as csvfile:
+     reader = csv.DictReader(csvfile)
+     for row in reader:
+        test_X.append(float(row['x'])/10)
+        test_Y.append(float(row['y'])/10)
+        
+train_X = np.array(train_X)
+train_Y = np.array(train_Y)
+test_X = np.array(test_X)
+test_Y = np.array(test_Y)
+
+
+n_samples = len(train_X)
+# tf Graph Input
+X = tf.placeholder("float")
+Y = tf.placeholder("float")
+
+# Set model weights
+W = tf.Variable(rng.randn(), name="weight")
+b = tf.Variable(rng.randn(), name="bias")
+
+# Construct a linear model
+pred = tf.add(tf.multiply(X, W), b)
+
+# Mean squared error
+cost = tf.reduce_sum(tf.pow(pred-Y, 2))/(2*n_samples)
+# Gradient descent
+#  Note, minimize() knows to modify W and b because Variable objects are trainable=True by default
+optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost)
+
+# Initialize the variables (i.e. assign their default value)
+init = tf.global_variables_initializer()
+
+# Start training
+with tf.Session() as sess:
+
+    # Run the initializer
+    sess.run(init)
+
+    # Fit all training data
+    for epoch in range(training_epochs):
+        for (x, y) in zip(train_X, train_Y):
+            sess.run(optimizer, feed_dict={X: x, Y: y})
+
+        # Display logs per epoch step
+        if (epoch+1) % display_step == 0:
+            c = sess.run(cost, feed_dict={X: train_X, Y:train_Y})
+            print("Epoch:", '%04d' % (epoch+1), "cost=", "{:.9f}".format(c), \
+                "W=", sess.run(W), "b=", sess.run(b))
+
+    print("Optimization Finished!")
+    training_cost = sess.run(cost, feed_dict={X: train_X, Y: train_Y})
+    print("Training cost=", training_cost, "W=", sess.run(W), "b=", sess.run(b), '\n')
+
+    # Graphic display
+    plt.plot(train_X, train_Y, 'ro', label='Original data')
+    plt.plot(train_X, sess.run(W) * train_X + sess.run(b), label='Fitted line')
+    plt.legend()
+    plt.show()
+
+    print("Testing... (Mean square loss Cokmparison)")
+    testing_cost = sess.run(
+        tf.reduce_sum(tf.pow(pred - Y, 2)) / (2 * test_X.shape[0]),
+        feed_dict={X: test_X, Y: test_Y})  # same function as cost above
+    print("Testing cost=", testing_cost)
+    print("Absolute mean square loss difference:", abs(
+        training_cost - testing_cost))
+
+    plt.plot(test_X, test_Y, 'bo', label='Testing data')
+    plt.plot(train_X, sess.run(W) * train_X + sess.run(b), label='Fitted line')
+    plt.legend()
+    plt.show()
