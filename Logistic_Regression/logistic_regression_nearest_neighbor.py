3a09d51320ce5a76fb0953395d2deca346193a0d
Joy <joydebmondal28@gmail.com>
2018-08-31 12:54:13 +0530

    update

--- Logistic_Regression/logistic_regression_nearest_neighbor.py
+++ Logistic_Regression/logistic_regression_nearest_neighbor.py
@@ -7,6 +7,7 @@ Created on Fri Aug 24 16:51:22 2018
 """
 
 import tensorflow as tf
+import numpy as np
 from tensorflow.examples.tutorials.mnist import input_data
 
 mnist = input_data.read_data_sets('../MNIST/',one_hot = True)
@@ -23,10 +24,22 @@ tr = tf.placeholder("float",[None,784])
 ts = tf.placeholder("float",[784])
 
 
+distance = tf.reduce_sum(tf.abs(tf.add(tr,tf.negative(ts))),reduction_indices=1)
 
+pred = tf.arg_min(distance,0)
 
+accuracy =0
+init = tf.global_variables_initializer()
 
-
+with tf.Session() as sess:
+    sess.run(init)
+    for i in range(len(x_test)):
+        nn_index = sess.run(pred,feed_dict={tr : x_train,ts : x_test[i,:]})
+        print("Test: ",i,", Prediction Class: ",np.argmax(y_train[nn_index]),", True Class: ",np.argmax(y_test[i]))
+        if np.argmax(y_train[nn_index]) == np.argmax(y_test[i]):
+            accuracy+=1/len(x_test)
+        
+    print("Accuarcy: ",accuracy)
 
 
 
