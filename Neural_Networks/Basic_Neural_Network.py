40cb1645673d565c2b61b8b4cdf9a28fe75e3aa9
Joy <joydebmondal28@gmail.com>
2018-08-31 18:15:33 +0530

    neural netwroks

---
+++ Neural_Networks/Basic_Neural_Network.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 31 17:58:45 2018
+
+@author: joy
+"""
+
+import tensorflow as tf
+import numpy as np
+from tensorflow.examples.tutorials.mnist import input_data
+
+mnist = input_data.read_data_sets('../MNIST/',one_hot = True)
+
+learning_rate = 0.1
+num_steps = 500
+batch_size = 128
+display_step = 100
+
+num_hidden_1 = 256
+num_hidden_2 = 256
+num_input = 784
+num_cls = 10 
+
+x = tf.placeholder('float',[None,num_input])
+y = tf.placeholder('float',[None,num_cls])
+
+weights = 
+        {
+        
+        }
+
+Bias = 
+        {
+                
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
