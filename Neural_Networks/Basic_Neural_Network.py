d29a849eb37d7389d090d0f5250bf28da3a2eed6
Joy <joydebmondal28@gmail.com>
2018-09-03 15:25:22 +0530

    neural net with 2 layer

--- Neural_Networks/Basic_Neural_Network.py
+++ Neural_Networks/Basic_Neural_Network.py
@@ -13,7 +13,7 @@ from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets('../MNIST/',one_hot = True)
 
 learning_rate = 0.1
-num_steps = 500
+num_steps = 5000
 batch_size = 128
 display_step = 100
 
@@ -41,7 +41,7 @@ bias = {
 
 
 
-def neural_net():
+def neural_net(x):
     
     layer1 = tf.add(tf.matmul(x,weights['h1']),bias['b1'])
     
@@ -53,15 +53,27 @@ def neural_net():
 
 logits = neural_net(x)
 
-loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits = logits,labels = y))
+loss_op = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits = logits,labels = y))
 optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
-train_op = optimizer.minimize(loss)
+train_op = optimizer.minimize(loss_op)
 
 correct_pred = tf.equal(tf.argmax(logits,1),tf.argmax(y,1))
 accuracy = tf.reduce_mean(tf.cast(correct_pred,tf.float32))
 
 init = tf.global_variables_initializer()
 
+
+with tf.Session() as sess:
+    sess.run(init)
+    for step in range(1,num_steps+1):
+        batch_x,batch_y = mnist.train.next_batch(batch_size)
+        sess.run(train_op, feed_dict = {x : batch_x, y:batch_y})
+        if num_steps%step ==0 :
+            loss,acc= sess.run([loss_op,accuracy], feed_dict = {x : batch_x, y:batch_y})
+    
+            print("Step " + str(step) + ", Minibatch Loss= " +"{:.4f}".format(loss) + ", Training Accuracy= " + "{:.3f}".format(acc))
+    
+    print("Testing Accuracy:",sess.run(accuracy, feed_dict={x: mnist.test.images, y: mnist.test.labels}))
 
 
 
