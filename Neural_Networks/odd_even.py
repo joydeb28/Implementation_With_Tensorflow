eac921bf46771625b3fe949773bf17ed070ffd4a
Joy <joydebmondal28@gmail.com>
2018-09-11 16:30:00 +0530

    params changes

--- Neural_Networks/odd_even.py
+++ Neural_Networks/odd_even.py
@@ -16,7 +16,7 @@ NUM_DIGITS = 10
 trX = np.array([binary_encode(i, NUM_DIGITS) for i in range(101, 2 ** NUM_DIGITS)])
 trY = np.array([encode(i) for i in range(101, 2 ** NUM_DIGITS)])
 
-NUM_HIDDEN = 100
+NUM_HIDDEN = 1000
 
 X = tf.placeholder("float", [None, NUM_DIGITS])
 Y = tf.placeholder("float", [None, 2])
@@ -43,35 +43,15 @@ predict_op = tf.argmax(py_x, 1)
 def predict(i, prediction):
     return ["odd", "even"][prediction]
 
-'''
-with tf.Session() as sess:
-    tf.initialize_all_variables().run()
-    for epoch in range(100):
-        p = np.random.permutation(range(len(trX)))
-        trX, trY = trX[p], trY[p]
-        
-        BATCH_SIZE = 128
-        for start in range(0, len(trX), BATCH_SIZE):
-            end = start + BATCH_SIZE
-            sess.run(train_op, feed_dict={X: trX[start:end], Y: trY[start:end]})
-            print(epoch, np.mean(np.argmax(trY, axis=1) ==
-                             sess.run(predict_op, feed_dict={X: trX, Y: trY})))
-            numbers = np.arange(1, 101)
-            teX = np.transpose(binary_encode(numbers, NUM_DIGITS))
-            teY = sess.run(predict_op, feed_dict={X: teX})
-            output = np.vectorize(predict)(numbers, teY)
-            save_path = saver.save(sess, "odd_even_models/model")
-            print("Model saved in path: %s" % save_path)
-            print(output)
-'''
+
 sess = tf.Session()
 init = tf.global_variables_initializer()
 sess.run(init)
-for epoch in range(100):
+for epoch in range(1000):
     p = np.random.permutation(range(len(trX)))
     trX, trY = trX[p], trY[p]
     
-    BATCH_SIZE = 128
+    BATCH_SIZE = 20
     for start in range(0, len(trX), BATCH_SIZE):
         end = start + BATCH_SIZE
         sess.run(train_op, feed_dict={X: trX[start:end], Y: trY[start:end]})
