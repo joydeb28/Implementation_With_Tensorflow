c44894ad61bd2ec995a41167bf57013a92fab30f
Joy <joydebmondal28@gmail.com>
2018-09-15 19:49:25 +0530

    BRNN

---
+++ Neural_Networks/Bidirectional_RNN.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 14 14:48:16 2018
+
+@author: joy
+"""
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep  3 19:06:38 2018
+
+@author: joy
+"""
+
+import tensorflow as tf
+from tensorflow.contrib import rnn
+
+
+
+
