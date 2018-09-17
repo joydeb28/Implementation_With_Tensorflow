56dccc8f8f396162dffe334c69efea006c7d0050
Joy <joydebmondal28@gmail.com>
2018-09-17 16:17:12 +0530

    bi-directional rnn updated

--- Neural_Networks/Bidirectional_RNN.py
+++ Neural_Networks/Bidirectional_RNN.py
@@ -16,6 +16,41 @@ Created on Mon Sep  3 19:06:38 2018
 
 import tensorflow as tf
 from tensorflow.contrib import rnn
+from tensorflow.examples.tutorials.mnist import input_data
+import numpy as np
+
+mnist = input_data.read_data_sets("../MNIST/",one_hot = True)
+
+learning_rate = 0.0001
+training_steps = 10000
+batch_size = 128
+display_size = 200
+
+num_input = 28
+timesteps = 28
+num_hidden = 128
+num_classes = 10
+
+X =  tf.placeholder('float',[None,timesteps,num_input])
+Y = tf.placeholder('float',[None,num_classes])
+
+
+weight = {
+                'out':tf.Variable(tf.random_normal([2*num_hidden,num_classes]))
+        }
+
+bias = {
+            'out' : tf.Variable(tf.random_normal([num_classes]))
+        }
+
+
+
+
+
+
+
+
+
 
 
 
